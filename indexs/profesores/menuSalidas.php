f9664a898d1d9ea41431f99a2ab1b4a5d9994e3d
JoaquinLorenzo07 <lorenzo.joaquin.et25@gmail.com>
2024-08-30 15:14:03 -0300

    re estilizacion

--- indexs/profesores/menuSalidas.php
+++ indexs/profesores/menuSalidas.php
@@ -3,43 +3,114 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Salidas Educativas</title>
-        <link rel="stylesheet" href="../../css/menuSalidas.css" type="text/css"></link>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
+        <title>Menu de Salidas</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            body {
+                background-color: white;
+                color: #fff;
+                text-align: center;
+            }
+            .container {
+                max-width: 900px;
+                margin-top: 50px;
+                padding: 30px;
+                background-color: white;
+                color: #000;
+            }
 
+            .form-control {
+                display: block;
+                width: 100%;
+                height: 34px;
+                padding: 6px 12px;
+                font-size: 14px;
+                line-height: 1.42857143;
+                color: #555;
+                background-color: #fff;
+                background-image: none;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+                -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+                -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+                transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+            }
+
+            .navbar-custom {
+                background-color: white;
+                padding: 10px;
+            }
+            .navbar-brand {
+                color: white;
+                font-weight: 400;
+                font-size: 30px;
+            }
+            .navbar-brand:hover {
+                color: #ffcc00;
+                cursor: pointer;
+            }
+            .btn-get-started {
+                background-color: #f8bf00;
+                color: #000;
+                border: none;
+            }
+            .btn-get-started:hover {
+                background-color: #ffcc00;
+            }
+            ul {
+                padding-left: 0;
+                list-style-type: none;
+            }
+            hr {
+                border: 1px solid #D6D0D0;
+                margin: 20px 0;
+            }
+            .botones {
+                margin-bottom: 5px;
+                max-width: 100%;
+                height: auto !important;
+                text-decoration: none !important;
+                cursor: pointer;
+                font-size: 17px;
+                color: #000;
+            }
+            .botones:focus { 
+                border: none;
+            }
+        </style>
+    </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-dark" style="color: white; padding: 10px;">
-            <div class="container-fluid d-flex align-items-center justify-content-between">
-                <div class="d-flex align-items-center">
-                    <img src="../../imagenes/eest.webp" style="height:80px;">
-                    <a class="navbar-brand" id="title" style="color: white; font-size:25px; margin-left: 30px;">Salidas Educativas</a>
-                </div>
-                <a href="profesores.php" class="btn btn-warning" style="color: white;">Atrás</a>
+        <nav class="navbar navbar-custom">
+            <div class="container-fluid d-flex align-items-center">
+                <button class="btn btn-warning ms-auto" style="color: white;">Atrás</button>
             </div>
         </nav>
-        <br>
 
-        <div class="d-flex align-items-center justify-content-center" style="height: calc(100vh - 120px);">
-            <div class="container-custom">
-                <header class="text-center">
-                    <h3 class="my-0 mt-3" style="color: black; padding: 10px;">Administrar Salidas Educativas</h3>
-                </header>
-
-                <button class="btn btn-primary btn-create">Crear nueva salida</button>
-                <h4>Pendientes</h4>
-                <div class="salida-list">
-                    <button class="btn btn-outline-primary salida-item">Salida n°1</button>
+        <div class="container">
+            <h1>(Nombre de la salida)</h1>
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <h3>Formularios</h3>
+                    <hr>
+                    <ul>
+                        <li><button class="btn w-100 mb-3">Anexo 5 / 8 / 9 / 10</button></li>
+                    </ul>
                 </div>
-
-                <button class="btn btn-secondary btn-history">Histórico de mis salidas</button>
+                <div class="col-md-6">
+                    <h3>Documentos (PDF)</h3>
+                    <hr>
+                    <ul>
+                        <li><button class="btn form-control botones w-100 mb-3">Anexo 4</button></li>
+                        <li><button class="btn form-control botones w-100 mb-3">Anexo 5</button></li>
+                        <li><button class="btn form-control botones w-100 mb-3">Anexo 8</button></li>
+                        <li><button class="btn form-control botones w-100 mb-3">Anexo 9</button></li>
+                    </ul>
+                </div>
             </div>
         </div>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybIVM2YBWrQQTg4lH/7f2P1UqlpHxG9hjMC4LbWRz4Pp0R8Z+" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-pDf7OALdM4Od8J7LvUsTfS7xkz+G3EC5R61b74pOUmGHxRcICJrwwC69az6LaNB4" crossorigin="anonymous"></script>
-        <script>
-        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
--- indexs/profesores/profesores.php
+++ indexs/profesores/profesores.php
@@ -20,30 +20,32 @@
         <title>Menu</title>
     </head>
     <body>
-
-        <a href="../../php/logout.php" class="btn btn-danger">Cerrar sesión</a>
-        <div class="row justify-content-center ">
-            <div class="col-6">
-                <h2 class="col-12 text-center mt-4">Opciones</h2>
-                <div class="col-12 text-center mt-4">
-                    <a href="menuSalidas.php" class="btn border-bottom border-top form-control" style="width: 100%;">Salidas Educativas</a>
+        <div class="container">
+            <a href="../../php/logout.php" class="btn btn-danger">Cerrar sesión</a>
+            <div class="row justify-content-center ">
+                <div class="col-6">
+                    <h2 class="col-12 text-center mt-4">Opciones</h2>
+                    <div class="col-12 text-center mt-4">
+                        <a href="menuSalidas.php" class="btn border-bottom border-top form-control" style="width: 100%;">Salidas Educativas</a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <h2 class="col-12 text-center mt-4">Hijos a Cargo</h2>
+                    <div class="col-12 text-center mt-4">
+                        <?php if (!empty($hijos)): ?>
+                            <?php foreach ($hijos as $hijo): ?>
+                                <a href="../salidasHijos.php?dniAlumno=<?= $hijo['dni'] ?>" class="btn border-bottom border-top form-control" style="width: 100%;"><?= $hijo['apellido'] . ' ' . $hijo['nombre'] ?></a><br><br>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <p>No hay hijos asociados.</p>
+                        <?php endif; ?>
+                        <?php if ($error): ?>
+                            <p><?= $error ?></p>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
-            <div class="col-6">
-                <h2 class="col-12 text-center mt-4">Hijos a Cargo</h2>
-                <div class="col-12 text-center mt-4">
-                    <?php if (!empty($hijos)): ?>
-                        <?php foreach ($hijos as $hijo): ?>
-                            <a href="../salidasHijos.php?dniAlumno=<?= $hijo['dni'] ?>" class="btn border-bottom border-top form-control" style="width: 100%;"><?= $hijo['apellido'] . ' ' . $hijo['nombre'] ?></a><br><br>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <p>No hay hijos asociados.</p>
-                    <?php endif; ?>
-                    <?php if ($error): ?>
-                        <p><?= $error ?></p>
-                    <?php endif; ?>
-                </div>
-            </div>
+        </div>
         <script src="../../librerias/jquery.js"></script>
         <script src="../../librerias/boostrap.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
