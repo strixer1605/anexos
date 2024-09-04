1c5bf195dcc2eacf4d517c62f9cc6f07b619a181
strixer1605 <strixer1605@gmail.com>
2024-09-04 20:54:08 -0300

    agregar persona anexo v

--- indexs/profesores/formularioAnexosCompletos.php
+++ indexs/profesores/formularioAnexosCompletos.php
@@ -280,6 +280,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script src="../../js/enviarFormularios.js"></script>
+        <script src="../../js/cargarPersonasAnexoV.js"></script>
         <script>
             $(document).ready(function(){
                 $('#dniSearch, #agregarPersona').on('keydown', function(event){
