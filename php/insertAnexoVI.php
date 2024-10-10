0d229e48e5a409a85208bf6e3a160dd7067c3448
strixer1605 <strixer1605@gmail.com>
2024-10-10 00:21:05 -0300

    formularios hijo arreglado

--- php/insertAnexoVI.php
+++ php/insertAnexoVI.php
@@ -10,12 +10,12 @@
     }
 
     $idSalida = $_SESSION['idSalida'];
-    $dniHijo = $_SESSION['dniHijo'];
+    $dniAlumno = $_SESSION['dniAlumno'];
     $domicilio = isset($_POST['domicilio']) ? trim($_POST['domicilio']) : '';
     $altura = isset($_POST['altura']) ? trim($_POST['altura']) : '';
     $localidad = isset($_POST['localidad']) ? trim($_POST['localidad']) : '';
 
-    $sqlCheck = "SELECT * FROM `anexovi` WHERE `dniAlumno` = '$dniHijo' AND `fkAnexoIV` = '$idSalida'";
+    $sqlCheck = "SELECT * FROM `anexovi` WHERE `dniAlumno` = '$dniAlumno' AND `fkAnexoIV` = '$idSalida'";
     $resultCheck = mysqli_query($conexion, $sqlCheck);
 
     if (mysqli_num_rows($resultCheck) > 0) {
@@ -26,18 +26,18 @@
             // Si son iguales, actualizar el registro existente
             $sqlUpdate = "UPDATE `anexovi` 
                         SET `domicilio` = '$domicilio', `altura` = '$altura', `localidad` = '$localidad' 
-                        WHERE `dniAlumno` = '$dniHijo' AND `fkAnexoIV` = '$idSalida'";
+                        WHERE `dniAlumno` = '$dniAlumno' AND `fkAnexoIV` = '$idSalida'";
             $resultado = mysqli_query($conexion, $sqlUpdate);
         } else {
             // Si el dniPadre es diferente, insertar un nuevo registro
             $sqlInsert = "INSERT INTO `anexovi`(`fkAnexoIV`, `dniAlumno`, `domicilio`, `altura`, `localidad`, `dniPadre`) 
-                        VALUES ('$idSalida', '$dniHijo', '$domicilio', '$altura', '$localidad', '$dniPadre')";
+                        VALUES ('$idSalida', '$dniAlumno', '$domicilio', '$altura', '$localidad', '$dniPadre')";
             $resultado = mysqli_query($conexion, $sqlInsert);
         }
     } else {
         // Si no existe el registro, insertar uno nuevo
         $sqlInsert = "INSERT INTO `anexovi`(`fkAnexoIV`, `dniAlumno`, `domicilio`, `altura`, `localidad`, `dniPadre`) 
-                    VALUES ('$idSalida', '$dniHijo', '$domicilio', '$altura', '$localidad', '$dniPadre')";
+                    VALUES ('$idSalida', '$dniAlumno', '$domicilio', '$altura', '$localidad', '$dniPadre')";
         $resultado = mysqli_query($conexion, $sqlInsert);
     }
 
