d49759f7e8987f12966b15d5c71e3a80973744c2
strixer1605 <strixer1605@gmail.com>
2024-12-04 18:32:52 -0300

    seccion de desarrolladores y BD actualizada

--- indexs/hijos/formularioAnexosPadres.php
+++ indexs/hijos/formularioAnexosPadres.php
@@ -44,6 +44,10 @@
             </div>
         </div>
 
+        <?php 
+            include('../../php/footer.php');
+        ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script src="../../js/enviarFormulariosPadres.js"></script>
